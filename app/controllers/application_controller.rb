5f2802b0b7718e247dbf1c460fa32a8b6fd8bbb9
Sean Tsai <seanseansean1223@hotmail.com>
2018-01-08 00:46:02 +0800

    Fix uncleared session error for clean build

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if !@current_user? and session[:user_id] and User.find(session[:user_id])
+      @current_user = User.find(session[:user_id])
   end
 
   def logged_in?
